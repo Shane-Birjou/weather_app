18aa39dc02aeaa7b94f9c43fb1aad66f3a1cf52d
Shane-Birjou <shanenbirjou@gmail.com>
2023-04-21 09:56:22 -0400

    Fixed api bugs

--- src/Api.js
+++ src/Api.js
@@ -8,11 +8,11 @@ export const geoApiOptions = {
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
-export const WEATHER_API_URL = "https://api.openweathermap.org/data/3.0";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
 
-export const WEATHER_API_KEY = '42b541d61bmshe040c8627388b7bp17a079jsnd3ab82312096';
+export const WEATHER_API_KEY = '900d1b6ab1187d2a6fbfea630fc96b91';
 
-//new '42b541d61bmshe040c8627388b7bp17a079jsnd3ab82312096'
-//old "900d1b6ab1187d2a6fbfea630fc96b91"
 
-//rory key 4729349bc2mshf02eaa371f052b9p1fa070jsnaeb997e74494
+
+
+// 900d1b6ab1187d2a6fbfea630fc96b91
